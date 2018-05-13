2536f8a6e27f6e3919666231298310dca8bc8107
Sokanas <v.jvenet@gmail.com>
2018-05-13 22:23:19 +1000

    Line Chart Base Initial
    Upload of Linechart clone of MS PowerBI linedot chart sample.

---
+++ Line Chart/.tmp/precompile/src/dataInterfaces.ts
@@ -0,0 +1,98 @@
+/*
+ *  Power BI Visualizations
+ *
+ *  Copyright (c) Microsoft Corporation
+ *  All rights reserved. 
+ *  MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the ""Software""), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *   
+ *  The above copyright notice and this permission notice shall be included in 
+ *  all copies or substantial portions of the Software.
+ *   
+ *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+/*
+    More or less copied from the Microsoft Example - modified to suit project
+
+*/
+
+module powerbi.extensibility.visual.lineBase7208B18920D946BCB1A3B34BF2CC8FA3  {
+    import SelectableDataPoint = powerbi.extensibility.utils.interactivity.SelectableDataPoint;
+    import IInteractivityService = powerbi.extensibility.utils.interactivity.IInteractivityService;
+    import IAxisProperties = powerbi.extensibility.utils.chart.axis.IAxisProperties;
+    import IValueFormatter = powerbi.extensibility.utils.formatting.IValueFormatter;
+
+    export class DateValue {
+        label: string;
+        value: number;
+
+        constructor(label: string, value: number) {
+            this.label = label;
+            this.value = value;
+        }
+    }
+
+    export interface LineDotPoint extends SelectableDataPoint {
+        dateValue: DateValue;
+        value: number;
+        dot: number;
+        sum: number;
+        highlight?: boolean;
+        opacity: number;
+        counter: any;
+    }
+
+    export interface Legend {
+        text: string;
+        transform?: string;
+        dx?: string;
+        dy?: string;
+    }
+
+    export interface ColumnNames {
+        category: string;
+        values: string;
+    }
+
+    export interface LineChartViewModel {
+        columnNames: ColumnNames;
+        dotPoints: LineDotPoint[];
+        settings: LineChartSettings;
+        dateMetadataColumn: DataViewMetadataColumn;
+        valuesMetadataColumn: DataViewMetadataColumn;
+        dateColumnFormatter: IValueFormatter;
+        dataValueFormatter: IValueFormatter;
+        dateValues: DateValue[];
+        isOrdinal: boolean;
+        yMinValue: number;
+        yMaxValue: number;
+        sumOfValues: number;
+        hasHighlights: boolean;
+    }
+
+    export interface MinMaxValue {
+        min: number;
+        max: number;
+    }
+
+    export interface LineChartDefaultSettingsRange {
+        dotSize: MinMaxValue;
+        lineThickness: MinMaxValue;
+        animationDuration: MinMaxValue;
+    }
+}
+
+
---
+++ Line Chart/node_modules/powerbi-visuals-utils-chartutils/CHANGELOG.md
@@ -0,0 +1,24 @@
+## 1.5.1
+* FIX: Was removed a wrong instruction from auto generated code that impacted on tests in visuals
+
+## 1.5.0
+* Added two new optional parameters for CreateScale function -- innerPadding and useRangePoint. The first lets set inner padding for scale instead of recieve it from constant. The second lets use rangePoint instead of rangeBands function for creation of ordinal scale.
+
+## 1.4.0
+* Remove width restriction of title in legend
+* Added new option to drawDefaultLabelsForDataPointChart function to control behavior of collided labels
+
+## 1.3.0
+* Updated packages
+
+## 1.2.0
+* Added 'disableNiceOnlyForScale' to 'CreateAxisOptions' interface
+and added verification with this property to createAxis func
+
+## 1.1.0
+* Removed `lodash`
+* Updated dependencies
+
+## 1.0.1
+* Update ChartUtils to use SVG utils version 1.0.0
+* Add CHANGELOG
