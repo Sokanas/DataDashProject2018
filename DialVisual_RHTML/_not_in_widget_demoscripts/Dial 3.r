64f217738a12df658393811658cc99f02259bf99
Sokanas <v.jvenet@gmail.com>
2018-08-12 16:10:07 +1000

    3 Dial Graph implementations
    2 in C3, 1 in GoogleVis
    Dial 5.r is my current favourite for continuation.
    
    All three are superior to that travesty that is the Plottly bullshit.
    
    I'll look at integrating the C3 Dial 5 into PBI on Monday.

---
+++ DialVisual_RHTML/_not_in_widget_demoscripts/Dial 3.r
@@ -0,0 +1,7 @@
+library(googleVis)
+
+Gauge <-  gvisGauge(CityPopularity, 
+                    options=list(min=0, max=800, greenFrom=500,
+                                 greenTo=800, yellowFrom=300, yellowTo=500,
+                                 redFrom=0, redTo=300, width=400, height=300))
+plot(Gauge)
