5863b1f8069cdd327d47f1bcd2d6fb2fda816bed
CJiangChen <38994966+CJiangChen@users.noreply.github.com>
2018-09-26 09:55:56 +1000

    DonutChart
    1

---
+++ donutChart original/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var donutChart3F46921B12354FB4BD9C29BA3171D391 = {
+        name: 'donutChart3F46921B12354FB4BD9C29BA3171D391',
+        displayName: 'donutChart',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.11.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.donutChart3F46921B12354FB4BD9C29BA3171D391.Visual(options),
+        custom: true
+    };
+}
---
+++ donutChart original/script.r
@@ -0,0 +1,27 @@
+source('./r_files/flatten_HTML.r')
+
+############### Library Declarations ###############
+libraryRequireInstall("ggplot2");
+libraryRequireInstall("plotly");
+libraryRequireInstall("magrittr")
+libraryRequireInstall("dplyr")
+
+####################################################
+
+################### Actual code ####################
+
+# Create test data.
+mtcars$manuf <- sapply(strsplit(rownames(mtcars), " "), "[[", 1)
+
+p <- mtcars %>%
+  group_by(manuf) %>%
+  summarize(count = n()) %>%
+  plot_ly(labels = ~manuf, values = ~count) %>%
+  add_pie(hole = 0.6) %>%
+  layout(title = "Donut charts using Plotly",  showlegend = F,
+         xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+         yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+
+####################################################
+
+internalSaveWidget(p, 'out.html');
